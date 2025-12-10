f8f817b5d08f50050a23c4a59487ef4855e74a14
KATHAN PATEL <kathanp2401@gmail.com>
2025-12-10 09:36:49 +0530

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Dashboard from "./dashboard.jsx";
-ReactDOM.render(<Dashboard />, document.getElementById("root")).render(
-    <React.StrictMode>
-        <Dashboard />
-    </React.StrictMode>
-)
- 
